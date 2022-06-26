7ab7c9525737a0357846de032ffaafd7034f1758
Siroj Matchanov <smatchanov@star.global>
2022-06-26 21:10:11 +0200

    fix: getContract endpoint should restrict access to only owned contracts

--- src/app.js
+++ src/app.js
@@ -2,19 +2,31 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {sequelize} = require('./model')
 const {getProfile} = require('./middleware/getProfile')
+const {Op} = require('sequelize');
+
 const app = express();
 app.use(bodyParser.json());
 app.set('sequelize', sequelize)
 app.set('models', sequelize.models)
 
 /**
- * FIX ME!
+ * Fetches the contract if it belongs to the user.
  * @returns contract by id
  */
 app.get('/contracts/:id',getProfile ,async (req, res) =>{
     const {Contract} = req.app.get('models')
     const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
+    const contract = await Contract.findOne({where: {
+        [Op.and]: [
+            {
+                id,
+                [Op.or]: [
+                    { ContractorId: req.profile.id },
+                    { ClientId: req.profile.id },
+                ]
+            }
+        ]
+    }})
     if(!contract) return res.status(404).end()
     res.json(contract)
 })
